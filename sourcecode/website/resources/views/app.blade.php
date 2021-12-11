1e31094d3545a1e567061233f117a37ef716b76b
Null <this.git@azate.email>
2021-12-11 18:30:18 +0200

    Rebuild front

--- sourcecode/website/resources/views/app.blade.php
+++ sourcecode/website/resources/views/app.blade.php
@@ -2,8 +2,7 @@
 
 @section('angular-styles')
     {{--angular styles begin--}}
-		<link rel="stylesheet" href="client/styles.41f9cd8f18e85618bcff.css" media="print" onload="this.media=&apos;all&apos;">
-		<link rel="stylesheet" href="client/styles.41f9cd8f18e85618bcff.css">
+		<link rel="stylesheet" href="client/styles.b29cfa4658be177d6ca0.css">
 	{{--angular styles end--}}
 @endsection
 
@@ -13,11 +12,11 @@
         var spinner = document.querySelector('.global-spinner');
         if (spinner) spinner.style.display = 'flex';
     }, 100);</script>
-		<script src="client/runtime-es2015.898157147968052a8b58.js" type="module"></script>
-		<script src="client/runtime-es5.898157147968052a8b58.js" nomodule defer></script>
-		<script src="client/polyfills-es5.7dec1fefa52cfcc5108b.js" nomodule defer></script>
-		<script src="client/polyfills-es2015.f93fa6be99734e20273f.js" type="module"></script>
-		<script src="client/main-es2015.07f483a3bbaeee2fcb1b.js" type="module"></script>
-		<script src="client/main-es5.07f483a3bbaeee2fcb1b.js" nomodule defer></script>
+		<script src="client/runtime-es2015.5b4eff44a04776dfe432.js" type="module"></script>
+		<script src="client/runtime-es5.5b4eff44a04776dfe432.js" nomodule defer></script>
+		<script src="client/polyfills-es5.85a146d3b567a2ddbb57.js" nomodule defer></script>
+		<script src="client/polyfills-es2015.394385f3043280af1d7f.js" type="module"></script>
+		<script src="client/main-es2015.6d0fabb23d604c86b9b3.js" type="module"></script>
+		<script src="client/main-es5.6d0fabb23d604c86b9b3.js" nomodule defer></script>
 	{{--angular scripts end--}}
 @endsection
